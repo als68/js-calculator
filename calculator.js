6777c3ada20713313586e3267cf63d6b5f57654a
root <suhandyl@gmail.com>
2016-06-07 15:51:53 -1000

    Finished all tests

--- calculator.js
+++ calculator.js
@@ -9,60 +9,57 @@
 var calculatorModule = (function() {
   var memory = 0;
   var total = 0;
-  /**
-   * sets the `total` to the number passed in
-   * @param  { Number } x
-   * @return { Number }    current total
-   */
+  function validationFunc(x){
+      if (typeof x !== "number") {
+      throw new Error("Argument is not a number");
+    }
+  }
+
   function _load(x){
+    validationFunc(x);
     total = x;
     return total;
   }
 
-function _getTotal(){
-  return total;
-}
+  function _getTotal(){
+    return total;
+  }
 
-function _add(x){
-  total = total + x;
-  return total;
-}
+  function _add(x){
+    validationFunc(x);
+    total = total + x;
+    return total;
+  }
 
-function _subtract(x){
-  total = total - x;
-  return total;
-}
+  function _subtract(x){
+    validationFunc(x);
+    total = total - x;
+    return total;
+  }
 
-function _multiply(x){
-  total = total * x;
-  return total;
-}
+  function _multiply(x){
+    validationFunc(x);
+    total = total * x;
+    return total;
+  }
 
-function _divide(x){
-  total = (total / x);
-  return total;
-}
+  function _divide(x){
+    validationFunc(x);
+    total = (total / x);
+    return total;
+  }
 
-function _recallMemory(){
-  return memory;
-}
+  function _recallMemory(){
+    return memory;
+  }
 
-function _saveMemory(){
-  memory = total;
-  return memory;
-}
+  function _saveMemory(){
+    memory = total;
+  }
 
-function _clearMemory(){
-  memory = 0;
-  return memory;
-}
-
-  /**
-   * Validation
-   */
-if (typeof x !== "number") {
-  console.log("Error");
-}
+  function _clearMemory(){
+    memory = 0;
+  }
 
   return {
     load: _load,
